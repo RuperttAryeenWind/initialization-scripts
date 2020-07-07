37ae3fbdfe72da7cab67feeeabaf80e74f19c49a
Rupertt Wind aka Aravind Anilkumar <ruperttaryeenwind@gmail.com>
2020-07-07 17:08:03 +0000

    Updated powerlevel9k to powerlevel10k

--- installation-scripts/install_shell.sh
+++ installation-scripts/install_shell.sh
@@ -5,10 +5,10 @@ brew cask install font-hack-nerd-font
 echo "ZPOWERLEVEL9K_MODE='nerdfont-complete'" >> ~/.zshrc
 
 # To install this theme for use in Oh-My-Zsh, clone this repository into your OMZ custom/themes directory.
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # You then need to select this theme in your ~/.zshrc:
-echo "ZSH_THEME='powerlevel9k/powerlevel9k'" >> ~/.zshrc
+echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> ~/.zshrc
 
 # Please note if you plan to set a POWERLEVEL9K_MODE to use a specific font, as described in this section of the Wiki, 
 # you must set the MODE before OMZ is loaded (look for source $ZSH/oh-my-zsh.sh in your ~/.zshrc).
